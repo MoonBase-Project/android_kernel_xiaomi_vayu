ffacb276bf557927883b5b4c9bfaa26608da6a58
Cyber Knight <cyberknight755@gmail.com>
2021-12-23 16:04:57 +0800

    fs/exec: Affine fingerprint service to big cpus
    Affining the fingerprint service to big CPUs would reduce latency.
    
    Signed-off-by: Cyber Knight <cyberknight755@gmail.com>
    [vayu]
    Signed-off-by: Carlos Jimenez (JavaShin-X) <javashin1986@gmail.com>

--- fs/exec.c
+++ fs/exec.c
@@ -81,6 +81,7 @@ static DEFINE_RWLOCK(binfmt_lock);
 #define HWCOMPOSER_BIN_PREFIX "/vendor/bin/hw/android.hardware.graphics.composer"
 #define ZYGOTE32_BIN "/system/bin/app_process32"
 #define ZYGOTE64_BIN "/system/bin/app_process64"
+#define FP_BIN_PREFIX "/vendor/bin/hw/android.hardware.biometrics.fingerprint@2.1-service.vayu"
 static struct signal_struct *zygote32_sig;
 static struct signal_struct *zygote64_sig;
 
@@ -1824,6 +1825,12 @@ static int do_execveat_common(int fd, struct filename *filename,
 					   HWCOMPOSER_BIN_PREFIX,
 					   strlen(HWCOMPOSER_BIN_PREFIX)))) {
 			current->flags |= PF_PERF_CRITICAL;
+			set_cpus_allowed_ptr(current, cpu_perf_mask);
+		}
+		else if (unlikely(!strncmp(filename->name,
+					   FP_BIN_PREFIX,
+					   strlen(FP_BIN_PREFIX)))) {
+		        current->flags |= PF_PERF_CRITICAL;
 			set_cpus_allowed_ptr(current, cpu_perf_mask);
 		}
 	}
